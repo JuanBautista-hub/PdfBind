616a306d4d34ca9f918a0d0b7fde162f0133c3cd
JuanBautista-hub <70982515+JuanBautista-hub@users.noreply.github.com>
2023-09-18 12:04:52 -0600

    Add files via upload

---
+++ UnirPdf/logica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnirPdf
+{
+    public class logica
+    {
+        private string folderOrigen{get; set;}
+        private string folderDestino { get; set; }
+        public string PedirRutaFolder()
+        {
+            Console.Write("Ingresa un ruta donde estan tus archivos pdf \n ");
+            folderOrigen = Console.ReadLine().Replace(" ", "");
+            while (folderOrigen == string.Empty)
+            {
+                Console.Write("Ingresa un ruta donde estan tus archivos pdf  \n");
+                folderOrigen = Console.ReadLine().Replace(" ", "");
+            }
+           
+        
+
+            return folderOrigen;
+
+        }
+
+        public string PreguntarMismoFolder()
+        {
+            Console.Write("¿Desea guardar en la misma ruta el resultado? \n");
+            Console.Write("Elige una opcion\n");
+            Console.Write("1: SI \n");
+            Console.Write("2: NO\n");
+            var  eleccion = Console.ReadLine().Replace(" ", "");
+
+            return eleccion;
+        }
+
+        public string PedirRutaFolderResultado()
+        {
+            Console.Write("Ingresa un ruta donde quieres guardar el resultado pdf  \n");
+             folderDestino = Console.ReadLine().Replace(" ", "");
+            while (folderDestino == string.Empty)
+            {
+                Console.Write("Ingresa un ruta donde quieres guardar el resultado pdf\n ");
+                folderDestino = Console.ReadLine().Replace(" ", "");
+            }
+
+
+            return folderDestino;
+        }
+
+    
+    }
+}
